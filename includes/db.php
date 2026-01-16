f8a85f02bf03f05446fec735422052217da9cb3b
adewale-codes <adewaleabiola78@gmail.com>
2026-01-16 20:45:02 +0000

    connected db

---
+++ includes/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$db   = "image_auth";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    die("DB Connection failed");
+}
